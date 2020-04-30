7519305424b1005ed1f9f8ad388b5052b970cb1e
Flamenrage <flamenrage@gmail.com>
2020-04-30 16:54:36 +0400

    Update PatientViewModel

--- ClinicBusinessLogic/ViewModels/PatientViewModel.cs
+++ ClinicBusinessLogic/ViewModels/PatientViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace ClinicBusinessLogic.ViewModels
@@ -8,9 +9,15 @@ namespace ClinicBusinessLogic.ViewModels
     {
         public int Id { get; set; }
 
+        [DisplayName("ФИО")]
+
         public string FIO { get; set; }
 
+        [DisplayName("Email")]
+
         public string Email { get; set; }
+
+        [DisplayName("Пароль")]
 
         public string Password { get; set; }
 
