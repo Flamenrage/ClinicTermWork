1506f46d9a97987806479cf832c36975eed4fced
Alexander-Goryaynov <gor.alex595@gmail.com>
2020-05-16 21:15:46 +0400

    Add AdministrationView project

---
+++ ClinicBusinessLogic/Interfaces/IMedicationLogic.cs
@@ -0,0 +1,23 @@
+﻿using ClinicBusinessLogic.BindingModels;
+using ClinicBusinessLogic.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClinicBusinessLogic.Interfaces
+{
+    public interface IMedicationLogic
+    {
+        List<MedicationViewModel> GetList();
+
+        List<MedicationViewModel> GetMostList();
+
+        MedicationViewModel GetElement(int id);
+
+        void AddElement(MedicationBindingModel model);
+
+        void UpdElement(MedicationBindingModel model);
+
+        void DelElement(int id);
+    }
+}
---
+++ ClinicBusinessLogic/Interfaces/IRequestLogic.cs
@@ -0,0 +1,17 @@
+﻿using ClinicBusinessLogic.BindingModels;
+using ClinicBusinessLogic.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClinicBusinessLogic.Interfaces
+{
+    public interface IRequestLogic
+    {
+        List<RequestViewModel> GetList();
+
+        RequestViewModel GetElement(int id);
+
+        int AddElement(RequestBindingModel model);
+    }
+}
