15ed7dc8c4e031a17a9576ae505b336ecc6afddc
Flamenrage <flamenrage@gmail.com>
2020-05-31 16:55:14 +0400

    Mail Configuration and models

---
+++ ClinicBusinessLogic/HelperModels/MailSendInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClinicBusinessLogic.HelperModels
+{
+    public class MailSendInfo
+    {
+        public string Email { set; get; }
+        public string Subject { set; get; }
+        public string Body { set; get; }
+        public string AttachmentPath { set; get; }
+    }
+}
